04bf57a0a74aee98ca1ee0c7cb653b49aad62895
Jaycom17 <juanorejuela499@gmail.com>
2025-01-23 16:32:55 -0500

    enero 23

--- src/messages.ts
+++ src/messages.ts
@@ -12,23 +12,14 @@ export const messages = [
         message: "Tu sonrisa es la cosa mas hermosa que he visto en mi vida",
         link: "https://open.spotify.com/intl-es/track/48svUiwMMYQRPyesVEnof1?si=4e6c1d28efae4d73"
     },{
-        message: "Cada mensaje tuyo es una sonrisa en mi rostro",
-        link: "https://open.spotify.com/intl-es/track/2GJyBJWPmnM7pK961cB6iV?si=d027d4a5d6a241c7"
-    },{
         message: "Mi corazón es solo tuyo mi amorcito precioso",
         link: "https://open.spotify.com/intl-es/track/6kdCN6gTWLcLxmLXoUcwuI?si=0a0b45d1fbbb49ed"
-    },{
-        message: "Estaré para ti en las buenas y en las malas",
-        link: "https://open.spotify.com/intl-es/track/0fHTVPRRKmWaRuIwrsNTMp?si=685ab473bc1c4613"
     },{
         message: "Te amo con todo mi corazón",
         link: "https://vt.tiktok.com/ZS6pmJqrv/"
     },{
         message: "Gracias por estar a mi lado en todo momento",
         link: "https://open.spotify.com/intl-es/track/2FkA1Pj0HaXh7sQb9zwkKh?si=979472dcfc5e490a"
-    },{
-        message: "Estoy loco por ti",
-        link: "https://vt.tiktok.com/ZS6pmjUHk/"
     },{
         message: "Te adoro mi amor",
         link: "https://open.spotify.com/intl-es/track/3lGMtkONrZdJ8kTCg6KIFf?si=914054937ebb4946"
@@ -54,29 +45,11 @@ export const messages = [
         message: "Acostarme pensando en ti alegra mis noches",
         link: "https://vt.tiktok.com/ZS6pa7CTt/"
     },{
-        message: "Eres el primer pensamiento de mi día",
-        link: "https://open.spotify.com/intl-es/track/1HdhWs2gq9L4D2fjNZsO4k?si=eec3b4d372ce410e"
-    },{
         message: "Eres el amor de mi vida",
         link: "https://vt.tiktok.com/ZS6pauupY/"
     },{
-        message: "Siempre estaré para ti sin importar nada",
-        link: "https://open.spotify.com/intl-es/track/05xgON7xWm3yN1yarMHdBv?si=50503c4504a34144"
-    },{
         message: "Ansío con locura que llegue la noche para verte",
         link: "https://open.spotify.com/intl-es/track/45xU99QgWETDFGhgivpYce?si=d6170b9b76d44221"
-    },{
-        message: "Eres la calma en mi tormenta",
-        link: "https://vt.tiktok.com/ZS6paQEoT/"
-    },{
-        message: "Tienes el poder de hacerme sonreír con solo una mirada",
-        link: "https://vt.tiktok.com/ZS6pmFTWT/"
-    },{
-        message: "Me haces sentir el hombre mas afortunado del mundo",
-        link: "https://vt.tiktok.com/ZS6paSS6W/"
-    },{
-        message: "Quiero compartir mi vida contigo",
-        link: "https://vt.tiktok.com/ZS6pmeGTm/"
     },{
         message: "Eres la dueña de mi corazón",
         link: "https://open.spotify.com/intl-es/track/0jr6tT2vc4cIFPHb6wufG3?si=b193cb81dbed4118"
@@ -87,4 +60,32 @@ export const messages = [
         message: "TE AMO MUCHISIMO MI AMOR",
         link: "https://vt.tiktok.com/ZS6paw2w9/"
     },
+    //----------------------
+    //Enero 23
+    {
+        message: "Vives hasta en mis sueños",
+        link: "https://vt.tiktok.com/ZS6sfh1wo/"
+    },{
+        message: "Si un dia te sientes sola, recuerda que siempre estaré para ti",
+        link: "https://vt.tiktok.com/ZS6sf8gqf/"
+    },{
+        message: "Si tu mundo se derrumba, yo estaré para reconstruirlo contigo",
+        link: "https://vt.tiktok.com/ZS6sfSXnk/"
+    },{
+        message: "El amor que siento por ti trasciende el tiempo y el espacio",
+        link: "https://vt.tiktok.com/ZS6sfLYLx/"
+    },{
+        message: "Si no logro tenerte recuerda que tu siempre me tendrás a mi",
+        link: "https://vt.tiktok.com/ZS6sfrTqp/"
+    },{
+        message: "Pueden pasar vidas enteras y aun asi mi corazón seguirá siendo tuyo",
+        link: "https://vt.tiktok.com/ZS6sfuKLU/"
+    },{
+        message: "Te elegiría mil veces",
+        link: "https://vt.tiktok.com/ZS6sfu6sP/"
+    },{
+        message: "En algún universo envejesemos juntos, oro porque sea en este",
+        link: "https://open.spotify.com/intl-es/track/0EhmTBq33eARyOz3SIi4P2?si=a35e35d92c744af3"
+    },
+    //----------------------
 ]
